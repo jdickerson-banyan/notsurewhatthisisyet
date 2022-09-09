295e505d41cb81dec61f957b2abf099aeca3149e
jdickerson-banyan <75698869+jdickerson-banyan@users.noreply.github.com>
2022-09-09 11:03:13 -0700

    express async handler

--- mern-nonsense/backend/controllers/goalController.js
+++ mern-nonsense/backend/controllers/goalController.js
@@ -1,23 +1,25 @@
-const getGoals = (req, res) => {
-    res.status(200).json({message: 'get goals'})
-}
+const asyncHandler = require('express-async-handler')
 
-const createGoal = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
+    res.status(200).json({message: 'get goals'})
+})
+
+const createGoal = asyncHandler(async (req, res) => {
     if (!req.body.test) {
         res.status(400)
         throw new Error('Text field needed')
     }
 
     res.status(200).json({message: 'create goal'})
-}
+})
 
-const editGoal = (req, res) => {
+const editGoal = asyncHandler(async (req, res) => {
     res.status(200).json({message: `edit goal ${req.params.id}`})
-}
+})
 
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
     res.status(200).json({message: `delete goal ${req.params.id}`})
-}
+})
 
 module.exports = {
     getGoals, createGoal, editGoal, deleteGoal
