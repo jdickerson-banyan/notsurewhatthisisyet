87cd120d99d660764038cd8cb8f55c5a0d047948
jdickerson-banyan <75698869+jdickerson-banyan@users.noreply.github.com>
2022-09-13 12:03:25 -0700

    registration form

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # notsurewhatthisisyet
 
-Currently uses Node, Express, MongoDB, Mongoose, React, Redux
+Currently uses Node, Express, MongoDB, Mongoose, React, Redux, TypeScript, CSS, HTML5
---
+++ mern-nonsense/frontend/src/pages/Register.tsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react"
+import { FaUser } from 'react-icons/fa'
+
+function Register() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    })
+
+    const { name, email, password, confirmPassword } = formData
+
+    const onChange = () => {
+
+    }
+
+    const onSubmit = () => {
+
+    }
+
+    return (
+        <>
+            <section className="heading"></section>
+            <h1>
+                <FaUser /> Register
+            </h1>
+            <p>Please create an account</p>
+            <section className="form">
+                <form onSubmit={onSubmit}>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="name"
+                            name="name"
+                            value={name}
+                            placeholder="Please enter your name"
+                            onChange={onChange} />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="email"
+                            className="form-control"
+                            id="email"
+                            name="email"
+                            value={email}
+                            placeholder="Please enter your email"
+                            onChange={onChange} />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="password"
+                            name="password"
+                            value={password}
+                            placeholder="Please choose a password"
+                            onChange={onChange} />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="password2"
+                            name="password2"
+                            value={confirmPassword}
+                            placeholder="Please confirm your password"
+                            onChange={onChange} />
+                    </div>
+                    <div className="form-group">
+                        <button type="submit" className="btn btn-block">Submit</button>
+                    </div>
+                </form>
+            </section>
+        </>
+    )
+}
+
+export default Register
